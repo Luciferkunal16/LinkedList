66f0b9edf3ef500f5b561fc4f19eab36ec83a0d0
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-03 16:38:37 +0530

    Added UC5 : Pop First Element from linked list

--- src/ImplementLinkedList.java
+++ src/ImplementLinkedList.java
@@ -4,10 +4,11 @@ package linkedList;
 public class ImplementLinkedList {
 	public static void main(String[] args) {
 		SingleyLinkedList list=new SingleyLinkedList();
-		list.append(56);
+		
 		list.append(30);
 		list.append(70);
 		list.insertBetweenValue(12, 2);
+		list.popFirstElement();
 		
 		list.display();
 		
--- src/SingleyLinkedList.java
+++ src/SingleyLinkedList.java
@@ -4,26 +4,24 @@ public class SingleyLinkedList {
 	Node head;
 	Node tail;
 
+	public void append(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			head.next = tail;
+		} else {
+			if (head.next == null) {
+				newNode.prev = head;
+				head.next = newNode;
+				tail = newNode;
+			} else {
+				tail.next = newNode;
+				newNode.prev = this.tail;
+				tail = newNode;
+			}
+		}
 
-
-	 public void append(int data) {
-	        Node newNode = new Node(data);
-	        if (head == null) {
-	            head = newNode;
-	            head.next = tail;
-	        } else {
-	            if (head.next == null) {
-	                newNode.prev = head;
-	                head.next = newNode;
-	                tail = newNode;
-	            } else {
-	                tail.next = newNode;
-	                newNode.prev = this.tail;
-	                tail = newNode;
-	            }
-	        }
-	     
-	    }
+	}
 
 	public Node push(int data) {
 		Node newNode = new Node(data);
@@ -38,23 +36,26 @@ public class SingleyLinkedList {
 		return newNode;
 
 	}
-	public void insertBetweenValue(int data,int  position) {
-		Node currNode=new Node(data);
-		currNode.data=data;
-		currNode.next=null;
-		
-		Node temp=head;
-		for(int i=0;i<position-1;i++)
-		{
-			temp=temp.next;
-			
+
+	public void insertBetweenValue(int data, int position) {
+		Node currNode = new Node(data);
+
+		Node temp = head;
+		for (int i = 0; i < position - 1; i++) {
+			temp = temp.next;
+
 		}
-		currNode.next=temp.next;
-		temp.next=currNode;
+		currNode.next = temp.next;
+		temp.next = currNode;
+	}
+
+	public void popFirstElement() {
 		
-		
-		
-		
+		if (head == null)
+             System.out.println("No Element in Linked LIst");
+		Node temp = head;
+		head = head.next;
+
 	}
 
 	public void display() {
